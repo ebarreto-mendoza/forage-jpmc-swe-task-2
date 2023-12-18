33f6bcceaef8b1216e89e09e102ab11523bd355d
ebarreto-mendoza <ebarreto-mendoza@ucmerced.edu>
2023-12-17 19:06:34 -0800

    Completed Task 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  //added showGraph property to control graph's visability
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +24,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      //initiating showGraph as false ~ we want the graph to show after user clicks on 'Start Streaming Data'
+      showGraph: false,
     };
   }
 
@@ -29,18 +33,29 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    //ensuring graph doesn't render until user activates button
+    if (this.state.showGraph){
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let x = 0;
+    const interval = setInterval( () => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state by creating a new array of data that consists of
+        // Previous data in the state and the new data from server
+        this.setState({ data: [...this.state.data, ...serverResponds], showGraph: true });
+      });
+
+      x++;
+      if(x > 1000){
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
